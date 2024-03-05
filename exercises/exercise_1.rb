2eaa3281665cafc9844b3e772b345988d5984131
Jonas-Kunz <jonas_K_7@outlook.com>
2024-03-04 22:20:34 -0400

    begin exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,10 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+  
+end
+touching_cloth = Store.create(name: "Touching Cloth", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+tatterSails = Store.create(name: "Tatter Sails Emporioum", annual_revenue: 1000000, mens_apparel: true, womens_apparel: true)
+puts Store.count
